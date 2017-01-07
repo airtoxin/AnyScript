becc0515f7c96c2f171149090443d4df43c58fc0
airtoxin <airtoxin@icloud.com>
2017-01-07 19:02:08 +0900

    Inject handler

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleActivation, activate, deactivate } from './actions';
 
-const App = ({ dispatch, active }) => (
+const App = ({ active, onClickToggle, onClickActivate, onClickDeactivate }) => (
   <div>
     <h1>Current: {active ? 'true' : 'false'}</h1>
-    <button onClick={() => dispatch(toggleActivation())}>Toggle</button>
-    <button onClick={() => dispatch(activate())}>Activate</button>
-    <button onClick={() => dispatch(deactivate())}>Deactivate</button>
+    <button onClick={onClickToggle}>Toggle</button>
+    <button onClick={onClickActivate}>Activate</button>
+    <button onClick={onClickDeactivate}>Deactivate</button>
   </div>
 );
 
-const selector = ({ app: { active } }) => {
-  return { active };
-};
-
-export default connect(selector)(App);
+export default connect(
+  state => ({
+    active: state.app.active,
+  }),
+  dispatch => ({
+    onClickToggle: () => dispatch(toggleActivation()),
+    onClickActivate: () => dispatch(activate()),
+    onClickDeactivate: () => dispatch(deactivate()),
+  }),
+)(App);
