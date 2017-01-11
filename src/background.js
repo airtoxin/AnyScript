bd0c737f31a25f526a56bcd065aa7d88b3b57852
airtoxin <airtoxin@icloud.com>
2017-01-11 09:04:25 +0900

    Exec script code

--- src/background.js
+++ src/background.js
@@ -1,27 +1,19 @@
-chrome.storage.sync.get('scripts', ({ scripts }) => {
-  if (!scripts) chrome.storage.sync.set({
-    scripts: [],
-  });
-});
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({
     url: "popup.html"
   });
 });
 
-let i = 0;
-setInterval(() => {
-  i++;
-  // chrome.storage.sync.set({
-  //   scripts: `i=${i}`,
-  // });
-}, 1000);
-
 chrome.runtime.onMessage.addListener(({ pageOpened }) => {
   if (!pageOpened) return;
 
-  chrome.tabs.executeScript({
-    code: `console.log('current i=${i}');`
+  chrome.storage.sync.get('state', ({ state }) => {
+    if (!state) return;
+
+    state.script.scripts.forEach(script => {
+      chrome.tabs.executeScript({
+        code: script.code,
+      });
+    });
   });
 })
--- src/view/store.js
+++ src/view/store.js
@@ -3,6 +3,13 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import middlewares from './middlewares';
 import reducer from './reducers';
 
+const syncState = (store) => {
+  const state = store.getState();
+  chrome.storage.sync.set({
+    state: state,
+  });
+}
+
 export default initialState => {
   const store = createStore(
     reducer,
@@ -10,12 +17,8 @@ export default initialState => {
     composeWithDevTools(applyMiddleware(...middlewares)),
   );
 
-  store.subscribe(() => {
-    const state = store.getState();
-    chrome.storage.sync.set({
-      state: state,
-    });
-  });
+  store.subscribe(() => syncState(store));
+  syncState(store);
 
   return store;
 };
