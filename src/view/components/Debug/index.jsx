307ccf5bc17e5a600c8e8d83ef3353ea85207e27
airtoxin <airtoxin@icloud.com>
2017-01-16 06:22:15 +0900

    Flow debug text

--- src/view/components/Debug/index.jsx
+++ src/view/components/Debug/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import util from 'util';
+import styles from './index.css';
 
 export default (props) => process.env.NODE_ENV === 'production' ? null : (
-  <pre>
+  <pre className={styles.debug}>
     {util.inspect(props, { depth: null })}
   </pre>
 );
